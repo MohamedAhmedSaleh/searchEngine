9ee45d40cae48e2397586ebb62f7c2707b2a65f7
Khaled sherif <khaledkeky@gmail.com>
2017-04-29 19:33:27 +0200

    dadsa

--- IR/Form1.Designer.cs
+++ IR/Form1.Designer.cs
@@ -132,7 +132,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1243, 535);
+            this.ClientSize = new System.Drawing.Size(446, 377);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.Bigram_Button);
             this.Controls.Add(this.label2);
